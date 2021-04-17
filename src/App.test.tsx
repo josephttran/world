b4fd924380889050e2f4596c9fd969556280c9fe
josephttran <42635012+josephttran@users.noreply.github.com>
2021-04-17 14:56:10 -0700

    Test needed to pass in order to complete automated deployment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders World App', () => {
-  render(<App />);
+  render(<BrowserRouter><App /></BrowserRouter>);
   const worldAppElement = screen.getByText(/World App/i);
   expect(worldAppElement).toBeInTheDocument();
 });
